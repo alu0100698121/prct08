c0c27a8e280af674646c6a6e3a23b676ef2b6975
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-11-04 15:43:00 +0000

    Creada expectativa para obtener valor de las columnas. Ejecucion testeada y correcta.

--- spec/matrices_spec.rb
+++ spec/matrices_spec.rb
@@ -2,11 +2,14 @@ require './lib/matrices.rb'
 
 describe Matriz do
 	before :each do
-		@m1 = Matriz.new(2)
+		@m1 = Matriz.new(2,2)
 	end
 	describe " # Almacenamiento de matrices. " do
 		it " # Se debe almacenar el numero de filas " do
 			@m1.N
+		end
+		it " # Se debe almacenar el numero de columnas " do
+			@m1.M
 		end
 	end
 	
