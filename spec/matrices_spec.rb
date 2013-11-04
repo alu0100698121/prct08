fe5b30a0ba4bf9b356172ca3de14edd6c61d2573
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-11-04 13:36:57 +0000

    Creada expectativa para comprobar numero de filas.

--- spec/matrices_spec.rb
+++ spec/matrices_spec.rb
@@ -1,1 +1,13 @@
 require './lib/matrices.rb'
+
+decribe Matriz do
+	before :each do
+		@m1 = Matriz.new(2,2)
+	end
+	describe " # Almacenamiento de matrices. " do
+		it " # Se debe almacenar el numero de filas " do
+			@m1.N
+		end
+	end
+	
+end
